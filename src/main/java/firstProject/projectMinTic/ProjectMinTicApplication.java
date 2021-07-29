82f2258f9b6053f24233ebc4aa8f280ea671fca5
Vanessa V <vanevb976@gmail.com>
2021-07-29 17:21:13 -0500

    first commit

---
+++ src/main/java/firstProject/projectMinTic/ProjectMinTicApplication.java
@@ -0,0 +1,13 @@
+package firstProject.projectMinTic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMinTicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMinTicApplication.class, args);
+	}
+
+}
